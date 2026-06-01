5a288a4cd8f24ff73316cbacab2bf7d9ef8d0770
PrzemyslawSagalo <przemek@sagalo.pro>
2026-06-01 22:41:03 +0200

    refactor: Install OpenShift client and set permissions
    Move the OpenShift client binary to /usr/local/bin and set executable permissions.

--- setup_dev_env/6_install_oc.sh
+++ setup_dev_env/6_install_oc.sh
@@ -5,3 +5,7 @@ set -e
 wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz
 tar -xvzf openshift-client-linux.tar.gz
 
+sudo mv oc /usr/local/bin/
+sudo chmod +x /usr/local/bin/oc
+
+rm -f openshift-client-linux.tar.gz kubectl README.md
